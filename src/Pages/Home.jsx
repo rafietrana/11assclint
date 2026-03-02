ffec840753da976232f33390b599efd3856c594c
rafietrana <bdmail674@gmail.com>
2026-03-02 08:44:32 +0600

    alhamdulillah i have sucessfully solved caseSencetive issue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,11 @@
 import BlogsSection from "../Component/BlogsSerction/BlogsSection";
-import JobsShowing from "../Component/jobsShowing/jobsShowing";
+
 
 import PopularCategory from "../Component/PopularCategory/PopularCategory";
 import Sliders from "../Component/sliders";
 import TopRecruter from "../Component/TopRecruter/TopRecruter";
 import WorkCounter from "../Component/WorkCounter/WorkCounter";
+import JobsShowing from "../Component/JobsShowing/JobsShowing";
 
 import NabBarAll from "./../Shyred/NabBarAll/NabBarAll";
 
